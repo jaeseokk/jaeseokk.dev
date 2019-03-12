12e6a70184e30107c95f093394c15d9967ab4247
jaeseok-kang <kang89kr@gmail.com>
2019-03-12 23:16:26 +0900

    Configure GA tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-136117159-1`,
       },
     },
     `gatsby-plugin-feed`,
