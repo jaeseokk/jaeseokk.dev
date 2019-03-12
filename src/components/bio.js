4b544d8f7b7d4a1b5b88c0247c422be361d96b85
jaeseok-kang <kang89kr@gmail.com>
2019-03-12 17:16:24 +0900

    Modify bio info

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,12 +38,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              Written by{' '}
+              <strong>
+                <a href={`https://twitter.com/${social.twitter}`}>{author}</a>
+              </strong>
             </p>
           </div>
         )
